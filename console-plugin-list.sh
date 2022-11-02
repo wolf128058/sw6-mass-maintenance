b89d88f14da8b38cb0c22c8e6a719404b8422ea2
Jonas Hess <jonas@sfxonline.de>
2022-11-02 22:40:13 +0100

    add script: console-plugin-list.sh

---
+++ console-plugin-list.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## filename     console-plugin-list.sh
+## description: run "console plugin:list" on the sw console of each shop
+##              listing your currently installed shopware plugins
+## author:      jonas@sfxonline.de
+## =======================================================================
+
+for row in $(cat data/shops.json | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+  echo
+  echo '-----------------'
+  echo $(_jq '.name')
+  echo '-----------------'
+  ssh $(_jq '.host') "$(_jq '.console') plugin:list"
+done
