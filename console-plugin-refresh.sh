9731c54f396f12f9e5c32ed77cc8eff8d26415bf
Jonas Hess <jonas@sfxonline.de>
2022-11-04 10:59:27 +0100

    new script: console-plugin-refresh.sh

---
+++ console-plugin-refresh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## filename     console-plugin-refresh.sh
+## description: run "console plugin:refresh" on the sw console of each shop
+##              refreshing the infoarmation
+##              about your currently installed shopware plugins
+## author:      jonas@sfxonline.de
+## =======================================================================
+
+for row in $(cat data/shops.json | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+  echo
+  echo '-----------------'
+  echo $(_jq '.name')
+  echo '-----------------'
+  ssh $(_jq '.host') "$(_jq '.console') plugin:refresh"
+done
